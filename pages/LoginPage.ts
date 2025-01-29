46dcc55ba1c92c6e66b43ac7ca1f4cc50eb60174
Thomas <przegralek.thomas@gmail.com>
2025-01-29 22:19:14 +0100

    Amélioration de la page de connexion

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -4,21 +4,39 @@ export class LoginPage {
 
     readonly page : Page
     readonly nameInput : Locator
-    readonly emailInput : Locator
+    readonly emailInputSignup : Locator
     readonly signupButton : Locator
     readonly newUserTitle : Locator
+    readonly loginAccountTitle : Locator
+    readonly emailInputLogin : Locator 
+    readonly passwordInput : Locator
+    readonly loginButton : Locator
+    readonly errorMessageLogin : Locator
+    readonly errorMessageSignup : Locator
 
     constructor(page : Page) {
         this.page = page
         this.nameInput = page.getByPlaceholder('Name')
-        this.emailInput = page.locator('[data-qa="signup-email"]')
+        this.emailInputSignup = page.locator('[data-qa="signup-email"]')
         this.signupButton = page.getByRole('button', {name:'Signup'})
         this.newUserTitle = page.locator('.signup-form > h2')
+        this.loginAccountTitle = page.locator('.login-form > h2')
+        this.emailInputLogin = page.locator('[data-qa="login-email"]')
+        this.passwordInput = page.getByPlaceholder("Password")
+        this.loginButton = page.getByRole('button', {name: 'Login'})
+        this.errorMessageLogin = page.locator('form[action="/login"] p');
+        this.errorMessageSignup = page.locator('form[action="/signup"] p');
     }
 
     async newUserSignup(nom:string, email:string) {
         await this.nameInput.fill(nom)
-        await this.emailInput.fill(email)
+        await this.emailInputSignup.fill(email)
         await this.signupButton.click()
+    }
+
+    async loginAccount(email:string, password: string) {
+        await this.emailInputLogin.fill(email)
+        await this.passwordInput.fill(password)
+        await this.loginButton.click()
     }
 }
