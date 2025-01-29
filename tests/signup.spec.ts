f785582f47db6284bb44431bc1b2a287530cf0e3
Thomas <przegralek.thomas@gmail.com>
2025-01-29 22:20:21 +0100

    Mise à jour des tests d'inscription pour couvrir de nouveaux scénarios

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -6,7 +6,6 @@ import {Navbar} from '../components/Navbar'
 import {AccountCreatedPage} from '../pages/AccountCreatedPage'
 import { DeleteAccountPage } from '../pages/DeleteAccountPage'
 
-
 test.describe('Register User', () => {
     let homePage : HomePage
     let signupPage : SignupPage
@@ -24,15 +23,16 @@ test.describe('Register User', () => {
         deleteAccountPage = new DeleteAccountPage(page)
         await homePage.visit()
         await homePage.popup()
+        await expect(homePage.titlePage).toBeVisible()
+        await navbar.menu('Signup / Login')
+
     })
 
-        test('Register User', async ({page}) => {
-            await expect(homePage.titlePage).toBeVisible()
-            await navbar.menu('Signup / Login')
+        test('Test Case 1: Register User', async ({page}) => { // Créer un nouvel utilisateur et le supprimer à la fin
             await expect(loginPage.newUserTitle).toBeVisible()
-            await loginPage.newUserSignup("Thomas", "tom.stockkk@gmail.com")
-            await expect (signupPage.nameInput).toHaveAttribute('value', 'Thomas'); // Ajout assertion personnel
-            await expect (signupPage.emailInput).toHaveAttribute('value', 'tom.stockkk@gmail.com'); // Ajout assertion personnel
+            await loginPage.newUserSignup("John", "johndoe596@gmail.com")
+            await expect (signupPage.nameInput).toHaveAttribute('value', 'John'); // Ajout assertion personnel
+            await expect (signupPage.emailInput).toHaveAttribute('value', 'johndoe596@gmail.com'); // Ajout assertion personnel
             await expect (signupPage.titleEnterAccountInformation).toBeVisible()
             await signupPage.enterAccountInformation()
             await expect(accountCreatedPage.titleAccountCreated).toBeVisible()
@@ -41,7 +41,12 @@ test.describe('Register User', () => {
             await navbar.menu("Delete Account")
             await expect(deleteAccountPage.titleDeleteAccount).toBeVisible()
             await deleteAccountPage.continue()
+        })
 
+        test('Test Case 5: Register User with existing email', async ({page}) => { // Essayer de créer un compte avec un utilisateur déjà existant , ne doit pas fonctionner
+            await expect (loginPage.newUserTitle).toBeVisible()
+            await loginPage.newUserSignup("John", "existingaddress@gmail.com")
+            await expect(loginPage.errorMessageSignup).toBeVisible()
         })
 
 })
